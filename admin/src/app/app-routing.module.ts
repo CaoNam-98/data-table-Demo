ffba50038c39ceb1552531ba529907845d91bea4
Cao Nam <caonam.uit@gmail.com>
2022-03-02 01:09:39 +0700

    fix router

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookDataComponent } from './book-data/book-data.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
   { path: 'books', component: BookDataComponent }
 ];
 
